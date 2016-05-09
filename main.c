6164b82594d8e6617e4505280101df05fef3a637
ejszymon <hd.kolodziejczak@gmail.com>
2016-05-09 19:39:40 +0200

    skrecanie
    zalezne od czujnika

--- main.c
+++ main.c
@@ -70,12 +70,80 @@ void diody_init(void)
 		GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 		GPIO_Init(GPIOD, &GPIO_InitStructure);
 }
+void Conf_GPIO() {
+	/* piny sterujace kierunkiem silników */
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* piny PWM dla serwa i silnika*/
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+}
+
+
+void Conf_Timer4() {
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	TIM_TimeBaseStructure.TIM_Period = 65500;
+	TIM_TimeBaseStructure.TIM_Prescaler = 83;
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+	TIM_Cmd(TIM4, ENABLE);
+}
+
+void Conf_PWM() {
+	TIM_OCInitTypeDef TIM_OCInitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStructure.TIM_Pulse = 0;
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+	/* KONFIGURACJA 1 KANALU */
+	TIM_OC1Init(TIM4, &TIM_OCInitStructure);
+	TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_TIM4);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	/* KONFIGURACJA 2 KANALU */
+	TIM_OC3Init(TIM4, &TIM_OCInitStructure);
+	TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_TIM4);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	GPIO_SetBits(GPIOB, GPIO_Pin_6);
+	GPIO_SetBits(GPIOB, GPIO_Pin_8);
+}
+
+void serwo_w_lewo(){ TIM4->CCR3 = 1550; }
+void serwo_w_prawo(){ TIM4->CCR3 = 950; }
+void serwo_standard(){ TIM4->CCR3 = 1250; }
 int main(void)
 {
 	SystemInit();
 
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+	Conf_GPIO(); // inicjalizacja portow GPIO
+	Conf_Timer4(); // konfiguracja zegara TIM4
+	Conf_PWM(); // inicjalizacja i konfiguracja PWM
 
+	przerwania_tim3_init();
 	diody_init();
 
 	TM_HCSR04_t czujnik;
@@ -101,14 +169,15 @@ int main(void)
 		        if (czujnik.Distance < 0) //blad
 		        {
 		        	GPIO_SetBits(GPIOD, czerwona);
+
 		        }
 		        else if (czujnik.Distance > 50) //ponad 50cm
 		        {
-		        	GPIO_SetBits(GPIOD, pomaranczowa);
+		        	serwo_w_prawo();
 		        }
 		        else //miedzy 0 a 50 cm
 		        {
-		        	GPIO_SetBits(GPIOD, niebieska);
+		        	serwo_w_lewo();
 		        }
 	}
 }
